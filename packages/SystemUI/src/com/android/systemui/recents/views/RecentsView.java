9af2c3bf06398f6b6669b6f81032ca865b3997d9
andrea <andre.turky@gmail.com>
2016-04-21 16:41:56 +0200

    Revert "Add clear all recents action to recents panel"
    This reverts commit 76426cbe63107df607469a60feb16b373734efa9.
    
    Conflicts:
    	packages/SystemUI/res/layout/recents.xml
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
    	packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -188,17 +188,6 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         return returnTask;
     }
 
-    public void dismissAllTasksAnimated() {
-        int childCount = getChildCount();
-        for (int i = 0; i < childCount; i++) {
-            View child = getChildAt(i);
-            if (child != mSearchBar) {
-                TaskStackView stackView = (TaskStackView) child;
-                stackView.dismissAllTasks();
-            }
-        }
-    }
-
     /** Launches the focused task from the first stack if possible */
     public boolean launchFocusedTask() {
         // Get the first stack view
@@ -418,6 +407,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         setMeasuredDimension(width, height);
     }
 
+
     public void noUserInteraction() {
         if (mClearRecents != null) {
             mClearRecents.setVisibility(View.VISIBLE);
@@ -435,6 +425,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             }
         });
     }
+
 
     /**
      * This is called with the full size of the window since we are handling our own insets.
